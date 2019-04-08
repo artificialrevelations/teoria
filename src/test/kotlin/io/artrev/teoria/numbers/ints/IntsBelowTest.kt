2fef95b76832d2070068b8fbc66e0c784611b20b
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2019-04-09 00:35:48 +0200

    Supplier unit test skeleton

---
+++ src/test/kotlin/io/artrev/teoria/numbers/ints/IntsBelowTest.kt
@@ -0,0 +1,14 @@
+package io.artrev.teoria.numbers.ints
+
+import org.junit.Assert.assertTrue
+import org.junit.experimental.theories.Theories
+import org.junit.experimental.theories.Theory
+import org.junit.runner.RunWith
+
+@RunWith(Theories::class)
+class IntsBelowTest {
+    @Theory
+    fun `all passed values are below`(@IntsBelow(value = 42) param: Int) {
+        assertTrue(param < 42)
+    }
+}
---
+++ src/test/kotlin/io/artrev/teoria/numbers/longs/LongsAboveTest.kt
@@ -0,0 +1,14 @@
+package io.artrev.teoria.numbers.longs
+
+import org.junit.Assert.assertTrue
+import org.junit.experimental.theories.Theories
+import org.junit.experimental.theories.Theory
+import org.junit.runner.RunWith
+
+@RunWith(Theories::class)
+class LongsAboveTest {
+    @Theory
+    fun `all passed values are above`(@LongsAbove(value = 42L) param: Long) {
+        assertTrue(param > 42L)
+    }
+}
